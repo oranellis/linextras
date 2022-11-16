0e467742ec70ec94ac6e613ae50cd80e2b422b94
Oran Ellis <oran.mcnair@gmail.com>
2022-11-16 20:37:42 +0000

    Made polybar config work between desktop and laptop

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,8 +7,12 @@ polybar-msg cmd quit
 # killall -q polybar
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log # /tmp/polybar2.log
-polybar 2>&1 | tee -a /tmp/polybar1.log & disown
-# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+if $(find /sys/class/power_supply | grep -i "bat0")
+then
+	polybar laptop 2>&1 | tee -a /tmp/polybar1.log & disown
+else
+	polybar desktop 2>&1 | tee -a /tmp/polybar2.log & disown
+fi
 
 echo "Bars launched..."
