7df684ce31d6c94e836292638e5e0acd0703110e
Oran Ellis <oran.mcnair@gmail.com>
2023-07-13 08:48:59 +0100

    Removed auto open markdown viewer, it was a bit annoying

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -426,6 +426,6 @@ vim.cmd([[highlight Normal guibg=NONE ctermbg=NONE]])
 -- =================== Plugin Settings ===================
 
 -- Markdown Viewer
-vim.g.mkdp_auto_start = 1
+vim.g.mkdp_auto_start = 0
 vim.g.mkdp_auto_close = 1
 vim.g.mkdp_theme = 'light'
