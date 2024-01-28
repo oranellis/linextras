6685d18d974cea90bfbfb7e1be26418c857d9857
Oran Ellis <oran.mcnair@gmail.com>
2024-01-28 17:44:52 +0000

    Fixed some ts highlighting issues

--- .config/nvim/after/plugin/nvim-treesitter.lua
+++ .config/nvim/after/plugin/nvim-treesitter.lua
@@ -2,11 +2,11 @@ require('nvim-treesitter.configs').setup {
 	modules = {},
 	ensure_installed = { "c", "cpp", "lua", "vim", "rust", "bash" },
 	ignore_install = {},
-	sync_install = false,
+	sync_install = true,
 	auto_install = true,
 	highlight = {
 		enable = true,
-		-- additional_vim_regex_highlighting = false,
+		additional_vim_regex_highlighting = true,
 	},
 	indent = {
 		enable = true,
