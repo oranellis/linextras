47cdd2c9e6032ddc7c120edf00ba48e5a24ab6dd
Oran Ellis <oran.mcnair@gmail.com>
2024-04-03 10:32:24 +0000

    Added auto capitalisation of git update args message

--- .config/git-update.sh
+++ .config/git-update.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  commit_msg="Update $(date '+%y%m%d %H%M')"
+	commit_msg="Update $(date '+%y%m%d %H%M')"
 else
-  commit_msg="$*"
+	remaining_args="$*"
+	if [[ ${remaining_args:0:1} =~ [a-zA-Z] ]]; then
+		# Capitalize the first letter and concatenate the rest of the string
+		commit_msg="${remaining_args^}"
+	else
+		commit_msg="$remaining_args"
+	fi
 fi
 
 git reset && git pull && git add -A && git commit -m "$commit_msg" && git push
