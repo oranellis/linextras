093e4543d1f1d3e799a3c1a5f92602d5358d99e9
Oran Ellis <oran.mcnair@gmail.com>
2024-05-01 12:52:18 +0100

    Improve keybind safety for nvim-tree if it doesn't load

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -9,7 +9,7 @@ local function on_attach(bufnr)
 	api.config.mappings.default_on_attach(bufnr)
 
 	-- custom mappings
-	vim.keymap.set('n', 'h', api.node.navigate.parent, opts('Parent Directory'))
+	vim.keymap.set("n", "h", api.tree.close, opts("Close"))
 	vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
 end
 
@@ -19,13 +19,18 @@ require("nvim-tree").setup({
 		sorter = "case_sensitive",
 	},
 	view = {
-		width = 30,
+		width = 40,
 	},
 	actions = {
+		change_dir = {
+			global = true,
+		},
 		open_file = {
 			quit_on_open = true,
 		},
 	},
 })
 
-vim.keymap.set('n', '<leader>n', vim.cmd.NvimTreeOpen, {})
+vim.keymap.set('n', '<leader>n', function ()
+	require("nvim-tree.api").tree.open()
+end)
